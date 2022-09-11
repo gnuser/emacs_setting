3cef313c5622008b175af45dfdf3a0c959da8a0b
Jing Chen <jing.chen@crypto.com>
2022-09-11 23:09:01 +0800

    Add ace-jump-mode and expand-region

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -6,6 +6,20 @@
   (open-line 1)
   (yank)
   )
+
 (global-set-key (kbd "C-c d") 'duplicate-line)
+
+(use-package ace-jump-mode
+  :bind ("C-c j" . ace-jump-mode)
+  :bind ("C-c u" . ace-jump-mode-pop-mark))
+
+(use-package expand-region
+  :bind ("M-=" . er/expand-region))
+
+;; macro
+;; 1. C-x ( kmacro-start-macro
+;; 2. C-x ) kmacro-end-macro
+;; 3. C-x e kmacro-end-and-call-macro
+;; 4. C-U 42 C-X E run macro 42 times
 
 (provide 'init-edit)
